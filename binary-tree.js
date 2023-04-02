4f5bf4885a3893c999f0d64464744585b9106f10
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-02 21:10:55 +0700

    Add buildTree and prettyPrint function

--- binary-tree.js
+++ binary-tree.js
@@ -1,8 +1,31 @@
 const Node = require("./node");
 
 class BinaryTree {
-    
-}
+  constructor(array) {
+    this.root = this.buildTree(array);
+  };
 
+  buildTree(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    if (sortedArray.length === 0) return null;
+
+    const midpoint = Math.floor(sortedArray.length / 2);
+    const newNode = Node(sortedArray[midpoint]);
+    newNode.leftChild = this.buildTree(sortedArray.slice(0, midpoint));
+    newNode.rightChild = this.buildTree(sortedArray.slice(midpoint + 1));
+    return newNode;
+  };
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) return;
+    if (node.rightChild) {
+      this.prettyPrint(node.rightChild, `${prefix}${isLeft ? '|   ' : '    '}`, false)
+    };
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.leftChild) {
+      this.prettyPrint(node.leftChild, `${prefix}${isLeft ? '    ' : '|   '}`, true)
+    };
+  };
+}
 
 module.exports = BinaryTree;
--- driver.js
+++ driver.js
@@ -1,2 +1,8 @@
-const BinaryTree = require("./binary-tree")
+const BinaryTree = require("./binary-tree");
+
+const randomArray = (size) => {
+  return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
+};
+
+const tree = new BinaryTree(randomArray(20));
 
