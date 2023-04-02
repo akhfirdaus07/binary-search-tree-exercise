b8219b03ff3a302a12b39364320c3e6c958ad1fb
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-02 21:27:47 +0700

    Add script in driver.js

--- driver.js
+++ driver.js
@@ -6,3 +6,24 @@ const randomArray = (size) => {
 
 const tree = new BinaryTree(randomArray(20));
 
+console.log(tree.isBalanced());
+
+console.log(tree.levelOrder());
+console.log(tree.inorder());
+console.log(tree.preorder());
+console.log(tree.postorder());
+
+tree.insert(300);
+tree.insert(400);
+tree.insert(500);
+
+console.log(tree.isBalanced());
+tree.rebalance();
+console.log(tree.isBalanced());
+
+console.log(tree.levelOrder());
+console.log(tree.inorder());
+console.log(tree.preorder());
+console.log(tree.postorder());
+
+tree.prettyPrint();
